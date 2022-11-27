756eddcb28264b72d2cf29d2cb7d8633f97179de
Eimantas Žilevičius <eimantaszilevicius@gmail.com>
2022-11-27 17:44:34 +0200

    Final project

---
+++ src/js/views/recipeview.js
@@ -0,0 +1,12 @@
+class Recipeview {
+  #parentElement = document.querySelector('.recipe');
+  #data;
+
+  render(data) {
+    this.#data = data;
+  }
+
+  #generateMarkup()
+}
+
+export default new Recipeview();
